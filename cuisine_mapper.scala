5534fb096262185ea1fff02ed4b03523800cfaa5
ANG ZHU <a7zhu@edu.uwaterloo.ca>
2019-12-30 02:33:54 -0500

    update cuisine_mapper.scala

--- cuisine_mapper.scala
+++ cuisine_mapper.scala
@@ -2,9 +2,6 @@ import scala.io.Source
 import scala.collection.immutable.HashMap
 import java.io._
 
-val file = "cuisine_map.txt"
-val writer = new BufferedWriter(new FileWriter(file))
-
 val cuisineMap = 
 Source.fromFile("YELP-Crawl-Run-2019-12-28T103701Z.csv", "ISO-8859-1").getLines.drop(1)
       .flatMap(rawTxtLine=>{
@@ -22,7 +19,9 @@ Source.fromFile("YELP-Crawl-Run-2019-12-28T103701Z.csv", "ISO-8859-1").getLines.
                 cuisineMapSoFar + (cuisine -> (cuisineMapSoFar(cuisine) :+ nameAddr)) 
            else cuisineMapSoFar + (cuisine -> List[(String,String)]((nameAddr)))
          })
-for(c<-cuisineMap){
-  writer.write(c.toString + "\n" + "=================================================================\n")
-}
+
+val file = "cuisine_map.txt"
+val writer = new BufferedWriter(new FileWriter(file))
+for(c<-cuisineMap)
+  writer.write(c.toString + "\n=================================================================\n")
 writer.close()
