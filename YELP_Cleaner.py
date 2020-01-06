801b6f63b8c40ce2bc1220a3a7f185d0d38a5d07
Ang Zhu <Aang@Angs-MacBook-Pro.local>
2020-01-05 20:21:39 -0500

    update YELP_Cleaner.py

--- YELP_Cleaner.py
+++ YELP_Cleaner.py
@@ -3,4 +3,4 @@ import geopy as gp
 
 pd.read_csv("YELP-Crawl-Run-2019-12-28T103701Z.csv", encoding='iso-8859-1') \
   .drop_duplicates() \
-  .to_csv("YELP_Clean.csv")
+  .to_csv("YELP_Clean.csv", index=False)
