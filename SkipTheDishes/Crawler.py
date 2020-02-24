9ef093b143e6b3a594ffe5e8fd7d09ac706ec2f1
A7ZH <a7zhu@edu.uwaterloo.ca>
2020-02-24 03:21:26 -0500

    DoorDash and SkipTheDishes

---
+++ SkipTheDishes/Crawler.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import time
+
+# Load and clean up locations, each being the representing address of a neighbourhood in Totonto.
+#   Prob1: What's the minumally necessary encoding needed for data in location.csv?
+#   Prob2: "Hood #" column is unecessary; removed.
+#   Prob3: "Unamed: 3" column is unecessary; removed.
+#   Prob4: Entries "Maple Leaf" & "Rouge" miss addresses; removed / manually fill in.
+#   Prob5: Entries contain address components that are invalid to UberEats address search, e.g. Unit #
+#          use err_ind=df.index[df['Repr Addr'].str.contains('error string')][0] to find the index.
+#          use df['Repr Addr'][err_ind]=df['Repr Addr'][err_ind][:x]+df['Repr Addr'][err_ind][y:] to modify.
+df=pd.read_csv('locations.csv', encoding='iso-8859-1').drop(columns=['Hood #'])
+df=df.loc[:, ~df.columns.str.contains('^Unnamed')]
+df=df.drop(df.index[df['Repr Addr'].isna()]).reset_index().drop(columns=['index'])
+
+# Add options for the Chrome browser.
+options=webdriver.ChromeOptions()
+options.binary_location='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+#options.add_argument('--headless')
+options.add_argument('--window-size=1920,1080')
+options.add_argument('--diable-extensions')
+options.add_argument('--diable-gpu')
+options.add_argument('--incognito')
+# Run the Chrome browser.
+driver=webdriver.Chrome(chrome_options=options)
+
+for addr in df['Repr Addr']:
+  print(addr)
+  # Navigate to "www.ubereats.com".
+  driver.get("https://www.skipthedishes.com/")
+  # Wait for the address search bar to become clickable, and click it. 
+  search_bar=WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,
+                                    "//input[@placeholder='Enter Your Address']")))
+  # Type the target address into search bar. Sometimes the address typed in gets truncated or cleared
+  #   as webpage refreshes itself while loading, thus we make 3 attempts to type in the target
+  #   address, interleaving with 1 second of wait time.
+  for i in range(3):
+    search_bar.click()
+    if(search_bar.get_attribute('value')==addr): pass
+    else:
+      while(search_bar.get_attribute('value')!=''):
+        search_bar.send_keys(Keys.ALT + Keys.RIGHT)
+        search_bar.send_keys(Keys.BACKSPACE)
+      search_bar.send_keys(addr)
+  time.sleep(1)
+  # Click the dropdown list for the target address 
+  driver.find_element_by_xpath("//div[@data-cy='OptionListItem']").click()
+  time.sleep(2)
+
+  # Click find restaurants nearby to jump 
+  '''
+  # Press return after inputting target address in to address search bar. Page jumps.
+  driver.find_element_by_xpath("//button[@data-cy='AddressEntry-Button']").click()
+  time.sleep(5)
+  # Wait for all the listing figures in the page to become visible.
+  figures = WebDriverWait(driver, 120).until(EC.visibility_of_all_elements_located((By.XPATH, 
+                                                                 "//figure[@height='240']")))
+  # Keep clicking "Show more" until no more new figures appear.
+  while True:
+    L=len(figures)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
+                                        "/html/body/div/div/div[3]/button"))).click()
+    time.sleep(4)
+    figures=WebDriverWait(driver, 120).until(EC.visibility_of_all_elements_located(
+                                            (By.XPATH, "//figure[@height='240']")))
+    if(len(figures)>L): pass
+    else: break
+  # Get all the listings when no more new listings show up by clicking "Show more". 
+  listings = driver.find_elements_by_xpath("//figure[@height='240']/ancestor::a")
+  # Create output file with the name being the Hood Name. 
+  filename = "Crawler_Output/" + df['Hood Name'][df.index[df['Repr Addr']==addr].values[0]]
+  output = open(filename,'w+')
+  # For each listing, scrape its text information and link for details page
+  for l in listings:
+    info1 = l.text.replace("\n", ";")
+    info2 = l.get_attribute('href')
+    print(info1 + '\n' + info2 + '\n', file=output)
+    print(info1 + '\n' + info2 + '\n')
+  output.close()
+  '''
+  driver.delete_all_cookies()
+driver.close()
